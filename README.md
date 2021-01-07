63693962a213b46c5556150b5a88ce7dece9ad33
SeungNyungLee <maple0110@naver.com>
2021-01-07 20:19:55 +0900

    [Momentum Clone Coding] VanillaJs를 이용한 클론 코딩

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React
+
+Vanilla JS를 이용한 momentum Example
---
+++ VanillaJS/Momentum/index.js
@@ -0,0 +1,5 @@
+const title = document.getElementById("title");
+
+title.innerHTML = 'HI! From JS';
+title.style.color = 'red';
+document.title = "I Own you now";
