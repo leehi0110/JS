f1a41212412ea9c2304e741b09cff262a183dc4d
SeungNyungLee <maple0110@naver.com>
2021-01-11 17:27:59 +0900

    [Add] React 기본을 이용한 Movie App 제작
    클래스형 컴포넌트와 함수형 컴포넌트를 사용해 영화정보를 받아 보여주는 앱 예제 개발
    
    Router를 사용해 를 사용해 url에 따라 화면전환이 이뤄질 수 있도록 설정

---
+++ ReactJS/movie/src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>
+        "Freedom is the freedom to say that plus two four. If that igrant, all else follows."
+      </span>
+      <span>George Orwell, 1984</span>
+    </div>
+  );
+};
+
+export default About;
