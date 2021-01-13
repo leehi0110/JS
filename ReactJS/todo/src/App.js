45c7ff1dd09332b1c045b68751445fd1c74723b4
SeungNyungLee <maple0110@naver.com>
2021-01-13 19:17:07 +0900

    [Update] useState의 함수형 업데이트를 이용해 성능 개선

--- ReactJS/todo/src/App.js
+++ ReactJS/todo/src/App.js
@@ -3,26 +3,22 @@ import TodoTemplate from './Components/TodoTemplate/TodoTemplate';
 import TodoInsert from './Components/TodoInsert/TodoInsert';
 import TodoList from './Components/TodoList/TodoList';
 
-function App() {
-  const [todos,setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링 해보기',
+const createBulkTodos = () => {
+  const array = [];
+  for(let i=1;i<=2500;i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
       checked: false,
-    }, 
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
-      checked: false,
-    },
-  ]);
+    });
+  }
+  return array;
+}
 
-  const nextId = useRef(4);
+function App() {
+  const [todos,setTodos] = useState(createBulkTodos);
+
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(
     text => {
@@ -31,27 +27,26 @@ function App() {
         text,
         checked: false,
       };
-
-      setTodos(todos.concat(todo));
+      setTodos(todos => todos.concat(todo));
       nextId.current += 1;
     },
-    [todos]
+    []
   );
 
   const onRemove = useCallback(
     id => {
-      setTodos(todos.filter(todo => todo.id !== id));
+      setTodos(todos => todos.filter(todo => todo.id !== id));
     },
-    [todos]
+    []
   );
 
   const onToggle = useCallback(
     id => {
       setTodos(
-        todos.map(todo => todo.id === id ? {...todo, checked: !todo.checked} : todo),
+        todos => todos.map(todo => todo.id === id ? {...todo, checked: !todo .checked} : todo)
       );
     },
-    [todos]
+    []
   );
 
   return (
