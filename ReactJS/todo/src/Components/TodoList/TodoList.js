a16c9bfb53b67b90eeaefc9b50f5a57287b22916
SeungNyungLee <maple0110@naver.com>
2021-01-13 19:54:29 +0900

    [Update] react-virtualized list 사용 list를 이용해 보이지 않는 리스트들을 미리 렌더링 하지 않도록 변경해 속도를 개선

--- ReactJS/todo/src/Components/TodoList/TodoList.js
+++ ReactJS/todo/src/Components/TodoList/TodoList.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+import { List } from 'react-virtualized';
 import TodoListItem from '../TodoListItem/TodoListItem';
 import './TodoList.scss';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+  const rowRenderer = useCallback(({index, key, style}) => {
+    const todo = todos[index];
+    return (
+      <TodoListItem
+        todo={todo}
+        key={key}
+        onRemove={onRemove}
+        onToggle={onToggle}
+        style={style}/>
+    );
+  },[onRemove, onToggle, todos]);
+
   return (
-    <div className="TodoList">
-      {todos.map(todo => (
-        <TodoListItem 
-          key={todo.id}
-          todo={todo}
-          onRemove={onRemove}
-          onToggle={onToggle}/>
-      ))}
-    </div>
+    <List
+      className="TodoList"
+      width={512}
+      height={513}
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRenderer}
+      list={todos}
+      style={{outline: 'none'}}/>
+    // <div className="TodoList">
+    //   {todos.map(todo => (
+    //     <TodoListItem 
+    //       key={todo.id}
+    //       todo={todo}
+    //       onRemove={onRemove}
+    //       onToggle={onToggle}/>
+    //   ))}
+    // </div>
   );
 };
 
