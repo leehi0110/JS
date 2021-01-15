2afb53ba5f771a9918e75abd9f7fda47fdd7bf6b
SeungNyungLee <maple0110@naver.com>
2021-01-15 14:28:04 +0900

    [Update] Using useAction hook

--- ReactJS/react-redux-tutorial/src/Containers/TodosContainer.js
+++ ReactJS/react-redux-tutorial/src/Containers/TodosContainer.js
@@ -2,25 +2,18 @@ import React, {useCallback} from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../Components/Todos';
+import useActions from '../lib/useAction';
 
-const TodosContainer = ({
-  input,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove,
-}) => {
+const TodosContainer = () => {
   const { input, todos } = useSelector(({todos}) => ({
     input: todos.input,
     todos: todos.todos
   }));
-  const dispatch = useDispatch();
-  const onChangeInput = useCallback(input => dispatch(changeInput(input)),[dispatch]);
-  const onInsert = useCallback(text => dispatch(insert(text)),[dispatch]);
-  const onToggle = useCallback(id => dispatch(toggle(id)),[dispatch]);
-  const onRemove = useCallback(id => dispatch(remove(id)),[dispatch]);
 
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    []
+  );
 
   return (
     <Todos
@@ -34,4 +27,4 @@ const TodosContainer = ({
   );
 };
 
-export default TodosContainer;
+export default React.memo(TodosContainer);
