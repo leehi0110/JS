138499f5f54b031f9d5da9619668972630a49c23
SeungNyungLee <maple0110@naver.com>
2021-01-15 13:35:31 +0900

    [Add+Update] Redux tutorial

---
+++ ReactJS/react-redux-tutorial/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CounterContainer from './Containers/CounterContainer';
+import TodosContainer from './Containers/TodosContainer';
+
+function App() {
+  return (
+    <div>
+      <CounterContainer/>
+      <hr/>
+      <TodosContainer/>
+    </div>
+  );
+}
+
+export default App;
